1fd787579685ea5d37fb6d9e88be78fade1ea301
posakya <roshanposakya07@gmail.com>
2018-11-27 20:13:23 +0545

    first commit

---
+++ app/src/main/java/com/grabcab/app/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.grabcab.app;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID ="AVsdGz11uDWbYg4DXwypcJ20w5bPFJQpgDRj_byw10jIB91SLbUt18yIRuwcBFv4rUFrUEJwPrvpordB";
+}
